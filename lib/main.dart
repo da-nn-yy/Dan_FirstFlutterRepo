39cd00a96128426a727079ea9f99295170ad5279
da-nn-yy <db7394928@gmail.com>
2024-07-02 23:57:55 +0200

    remove debug Banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme:
             ThemeData(primaryColor: Colors.purple[900], fontFamily: 'Poppins'),
         home: DanRandoms());
