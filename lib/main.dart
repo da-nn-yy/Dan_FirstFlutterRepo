a70697b57faa801dfde06954aad61ec20953fd82
da-nn-yy <db7394928@gmail.com>
2024-07-02 19:04:34 +0200

    myApps with new 'hello world' in the center

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.purple[900]),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dany Friends'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
